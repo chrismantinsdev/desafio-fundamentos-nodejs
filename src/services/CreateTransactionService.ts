e97ecef262054cb7fea1d21a402a85a1f560ad95
chrismantinsdev <chris.mantins.dev@gmail.com>
2020-05-14 04:36:42 -0300

    first commit to send challanger files

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,32 @@
+import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value)
+      throw Error('You dont have balance to make transaction');
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
